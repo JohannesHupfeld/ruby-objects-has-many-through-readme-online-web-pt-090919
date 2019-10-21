d8b95fec6875b78f3ab1fc3816bf529ac43d7539
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-10-21 05:17:33 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -10,6 +10,12 @@ class Waiter
     @@all << self
   end
  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+   end
+  end
+ 
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
